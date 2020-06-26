201604ce01e88239ebfab651281482f807ac2903
Miraclx <omiraculous@gmail.com>
2020-06-26 05:38:41 +0100

    Update typings to properly include type info for the props

--- typings/lib.d.ts
+++ typings/lib.d.ts
@@ -2,6 +2,9 @@ export = stringd;
 
 declare namespace stringd {
   export interface ParsedString<T> extends String { }
+  export interface PropSchema {
+    [key: string]: string;
+  }
 }
 
 /**
@@ -10,5 +13,5 @@ declare namespace stringd {
  * @param props Object containing the properties for variables to be replaced with
  * @param ignore Tags within a snippet of a string to be ignored. (used specifically to avoid tag repetition/recursion)
  */
-declare function stringd<T = {}>(tmp: string, props: T): stringd.ParsedString<T>;
-declare function stringd<T = {}>(tmp: string, props: T, ignore: string[]): stringd.ParsedString<T>;
+declare function stringd<T = stringd.PropSchema>(tmp: string, props: T): stringd.ParsedString<T>;
+declare function stringd<T = stringd.PropSchema>(tmp: string, props: T, ignore: string[]): stringd.ParsedString<T>;
